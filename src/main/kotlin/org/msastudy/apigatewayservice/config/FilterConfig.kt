15bcd9f4c7468e74b8082e46366dcf23db0fb843
Luke(김기정) <kkj@gabia.com>
2021-07-28 16:00:23 +0900

    Feat: GlobalFilter.kt 추가

--- src/main/kotlin/org/msastudy/apigatewayservice/config/FilterConfig.kt
+++ src/main/kotlin/org/msastudy/apigatewayservice/config/FilterConfig.kt
@@ -7,17 +7,17 @@ import org.springframework.context.annotation.Configuration
 
 @Configuration
 class FilterConfig {
-    @Bean
-    fun gatewayRoutes(builder : RouteLocatorBuilder): RouteLocator {
-        return builder.routes()
-            .route { r -> r.path("/first-service/**")
-                    .filters { f -> f.addRequestHeader("first-request", "first-request-header")
-                                     .addResponseHeader("first-response", "first-response-header") }
-                    .uri("http://localhost:8081") }
-            .route { r -> r.path("/second-service/**")
-                .filters { f -> f.addRequestHeader("second-request", "second-request-header")
-                    .addResponseHeader("second-response", "second-response-header") }
-                .uri("http://localhost:8082") }
-            .build()
-    }
+//    @Bean
+//    fun gatewayRoutes(builder : RouteLocatorBuilder): RouteLocator {
+//        return builder.routes()
+//            .route { r -> r.path("/first-service/**")
+//                    .filters { f -> f.addRequestHeader("first-request", "first-request-header")
+//                                     .addResponseHeader("first-response", "first-response-header") }
+//                    .uri("http://localhost:8081") }
+//            .route { r -> r.path("/second-service/**")
+//                .filters { f -> f.addRequestHeader("second-request", "second-request-header")
+//                    .addResponseHeader("second-response", "second-response-header") }
+//                .uri("http://localhost:8082") }
+//            .build()
+//    }
 }
---
+++ src/main/kotlin/org/msastudy/apigatewayservice/filter/GlobalFilter.kt
@@ -0,0 +1,43 @@
+package org.msastudy.apigatewayservice.filter
+
+import org.msastudy.apigatewayservice.util.Log
+import org.springframework.cloud.gateway.filter.GatewayFilter
+import org.springframework.cloud.gateway.filter.GatewayFilterChain
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory
+import org.springframework.stereotype.Component
+import org.springframework.web.server.ServerWebExchange
+import reactor.core.publisher.Mono
+
+@Component
+class GlobalFilter : AbstractGatewayFilterFactory<GlobalFilter.Config>(Config::class.java) {
+    companion object : Log
+
+    data class Config(
+        var baseMessage: String,
+        var preLogger: Boolean,
+        var postLogger: Boolean
+    )
+
+    override fun apply(config: Config): GatewayFilter {
+        logger.info("apply used!")
+        return GatewayFilter { exchange: ServerWebExchange, chain: GatewayFilterChain ->
+            val request = exchange.request
+            val response = exchange.response
+
+            logger.info("Global Filter baseMessage: {}", config.baseMessage)
+
+            if (config.preLogger) {
+                logger.info("Global Filter Start: request id -> {}", request.id)
+                println(request.id)
+            }
+
+            chain.filter(exchange).then<Void?>(Mono.fromRunnable {
+                logger.info(
+                    "Global Filter End: response code -> {}",
+                    response.statusCode
+                )
+                println(response.statusCode)
+            })
+        }
+    }
+}
